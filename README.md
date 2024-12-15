ed0e51ddf8080f1ba254f3923aa012d9c4b2a051
Flowd1T <flowdit@DESKTOP-T4K03G6.>
2024-12-15 23:54:39 +0700

    upd 3

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Flowd1T/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Flowd1T/python-project-49/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/7357744bdb78dd45aac4/maintainability)](https://codeclimate.com/github/Flowd1T/python-project-49/maintainability)
+
+Brain even:
+https://asciinema.org/a/dOLADFpxf7jtUWtFwFnJ5dHqe
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,28 @@
+from random import randint
+
+def game_even():
+    i = 0
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f'Hello, {name}!')
+
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while i < 3:
+        num = randint(0, 100)
+        print(f'Question: {num}')
+        answer = input('Your answer: ')
+        if num % 2 == 0 and answer.lower() == 'yes':
+            print('Correct!')
+            i += 1
+        elif num % 2 != 0 and answer.lower() == 'no':
+            print('Correct!')
+            i += 1
+        else:
+            print(f"Let's try again, {name}")
+    print(f'Congratulations, {name}!')
+
+def main():
+    game_even()    
+
+if __name__ == '__main__':
+    main()
