c15781db366eb6462be48f88a807d94f3b277fdd
Flowd1T <flowdit@DESKTOP-T4K03G6.>
2024-12-16 20:34:09 +0700

    Upd 6

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 [![Actions Status](https://github.com/Flowd1T/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Flowd1T/python-project-49/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7357744bdb78dd45aac4/maintainability)](https://codeclimate.com/github/Flowd1T/python-project-49/maintainability)
 
-Brain even:
-https://asciinema.org/a/dOLADFpxf7jtUWtFwFnJ5dHqe
-Brain calc:
-https://asciinema.org/a/n4hDXgjgUdpftDJmW5jOdc6NA
-Brain gcd:
-https://asciinema.org/a/EDG7OrnuK2O3bmtapfxQCTNAp
-Brain progression:
-https://asciinema.org/a/J7fjtbhrcsDTHZNbJYmGqVa2G
+Brain even: https://asciinema.org/a/dOLADFpxf7jtUWtFwFnJ5dHqe
+Brain calc: https://asciinema.org/a/n4hDXgjgUdpftDJmW5jOdc6NA
+Brain gcd: https://asciinema.org/a/EDG7OrnuK2O3bmtapfxQCTNAp
+Brain progression: https://asciinema.org/a/J7fjtbhrcsDTHZNbJYmGqVa2G
+Brain prime: https://asciinema.org/a/b76FzlKrCBJcXvcoKsVdypTkd
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,36 @@
+from random import randint
+
+def IsPrime(n):
+    d = 2
+    while n % d != 0:
+        d += 1
+    return d == n
+def game_prime():
+    i = 0
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f'Hello, {name}!')
+
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    while i < 3:
+        num_question = randint(2, 100)
+        print(f"Question: {num_question}")
+        answer = input('Your answer: ')
+        if IsPrime(num_question) == True and answer.lower() == 'yes':
+            print('Correct!')
+            i += 1
+        elif IsPrime(num_question) == False and answer.lower() == 'no':
+            print('Correct!')
+            i += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{"yes" if IsPrime(num_question) == True else "no"}'.")
+            print(f"Let's try again, {name}!")
+
+    print(f'Congratulations, {name}!')
+
+def main():
+    game_prime()
+
+if __name__ == '__main__':
+    main()
