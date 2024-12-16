39fd313c39ee9345450faa2b2ad9a236b27b1de1
Flowd1T <flowdit@DESKTOP-T4K03G6.>
2024-12-16 14:50:04 +0700

    upd 4

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,41 @@
+import random
+
+def game_calc():
+    i = 0
+
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f'Hello, {name}!')
+
+    while i < 3:
+
+        operand = ['+', '-', '*']
+        first_num = random.randint(0, 100)
+        second_num = random.randint(0, 100)
+        rand_operand = random.choice(operand)
+        question = str(first_num) + ' ' + rand_operand + ' ' + str(second_num)
+
+        print('What is the result of the expression?')
+        print(f'Question: {question}')
+        answer = input('Your answer: ')
+        answer_int = int(answer)
+
+        if rand_operand == '*':
+            answer_true = int(first_num) * int(second_num)
+        elif rand_operand == '+':
+            answer_true = int(first_num) + int(second_num)
+        elif rand_operand == '-':
+            answer_true = int(first_num) - int(second_num)
+
+        if answer_int == answer_true:
+            print('Correct!')
+            i += 1
+        else:
+            print(f"'{answer_int}' is wrong answer ;(. Correct answer was '{answer_true}'.")
+            print(f"Let's try again, {name}!")
+
+def main():
+    game_calc()
+
+if __name__ == '__main__':
+    main()
